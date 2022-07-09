e13e7e8fcd776def22835dc272886ba8517ce983
LARASPY <108844702+LARASPY@users.noreply.github.com>
2022-07-09 11:18:59 +0800

    修复hentai搜索&菜单消失问题

--- xhua.js
+++ xhua.js
@@ -4,7 +4,7 @@
 // @name:zh-TW   圖聚合展示by xhua
 // @name:en      Image aggregation display by xhua
 // @namespace    https://greasyfork.org/zh-CN/scripts/442098-%E5%9B%BE%E8%81%9A%E5%90%88%E5%B1%95%E7%A4%BAby-xhua
-// @version      3.92
+// @version      3.93
 // @description  目标是聚合网页美女图
 // @description:zh-TW 目標是聚合網頁美女圖
 // @description:en  The goal is to aggregate web beauty images
@@ -1456,7 +1456,7 @@ function popUpMenu() {
                 $('iframe').remove(); //移除广告等无必要元素
                 $("div #social_button").remove();
                 $("div #top_ad").remove();
-                $("#header h1").nextAll().remove();
+                $("#header .right-menu").nextAll().remove();
             },100);
         }).switchAggregationBtn(function () {
             activateFancyBox();
