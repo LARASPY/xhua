3eaabecbf13d407227e94aa82d338756876ceef2
LARASPY <108844702+LARASPY@users.noreply.github.com>
2022-10-07 14:57:10 +0800

    修复 slidingPosition 移位 BUG

--- other/slidingPosition@v_0.2.js
+++ other/slidingPosition@v_0.2.js
@@ -28,41 +28,48 @@ const configObserver = {
   attributeFilter: ["class"],
 };
 // 当观察到突变时执行的回调函数
-const Callbacks = function (mutationsList) {
+const callbacks = function (mutationsList) {
   mutationsList.forEach(function (item, index) {
-    // log_(type(item.type) + " " + item.type);
-    if ("attributes" == item.type) {
+    // log_(' # ',type(item.type) + " " + item.type);
+    if ("attributes" === item.type) {
       if (
-        item.target.className ==
-        "fancybox__slide has-image can-zoom_in is-selected"
+        item.target.className ===
+        "fancybox__carousel is-draggable"
       ) {
-        log_(item);
-        addEvent(item);
+        log_(' # ', item);
+        openEvent(item);
+      } else if (
+        item.target.className ===
+        "fancybox__container is-animated is-closing"
+      ) {
+        log_(' # ', item);
+        closeEvent();
       }
     }
   });
 };
 // 创建一个链接到回调函数的观察者实例
-const Observer = new MutationObserver(Callbacks);
+const Observer = new MutationObserver(callbacks);
 ContentContainer && Observer.observe(ContentContainer, configObserver);
-function addEvent(item) {
+function openEvent(item) {
   slideIndex =
-    item.target.parentElement.parentElement.parentElement.parentElement
-      .children[1].firstElementChild.firstElementChild.firstChild.innerText - 1;
+    item.target.offsetParent.childNodes[1].firstChild.firstChild
+      .childNodes[0].innerText - 1;
   if (slideIndex) {
     log_("open - # " + slideIndex + " slide is open!");
   }
 }
-
-function destroyFun(fancybox, slide) {
-    log_("close - # " + slideIndex + " slide is closed!");
+function closeEvent() {
+  log_("close - # " + slideIndex + " slide is closed!");
   let elementById = document.getElementById("imgLocation" + slideIndex);
   if (elementById) {
     elementById.scrollIntoView({
       block: "center",
-      behavior: "auto",
+      behavior: "smooth",
       inline: "center",
     });
+  } else {
+    console.error(" # ", "未定位id！");
   }
 }
 
@@ -75,11 +82,6 @@ new Promise(function (resolve) {
   }, 100);
 }).then(function () {
   Fancybox4.bind("[data-fancybox='images']", {
-    Thumbs: { Carousel: { fill: false, center: true } },
-    on: {
-      destroy: (fancybox, slide) => {
-        destroyFun(fancybox, slide);
-      },
-    },
+    Thumbs: { Carousel: { fill: false, center: true } }
   });
 });
